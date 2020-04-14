5d9e075b196bf2836e27e5e847e8e8257170ed58
JasonCharlesChan <chenqingze1987@gmail.com>
2020-04-14 20:19:45 +0800

    first commit

--- src/app/im/model/msg.model.ts
+++ src/app/im/model/msg.model.ts
@@ -1,4 +1,8 @@
 import {Message} from './message.model';
+import {PacketType} from './packet-type.enum';
 
 export class Msg extends Message {
+  constructor(packetType: PacketType, seq: number, payloadLength: number) {
+    super(packetType, seq, payloadLength);
+  }
 }
