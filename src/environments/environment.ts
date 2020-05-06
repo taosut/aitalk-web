ddcd7aae1c8441fe0cb8cca3d9c7924843585ac4
JasonCharlesChan <chenqingze1987@gmail.com>
2020-05-06 19:08:16 +0800

    fix websocket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ import {WebSocketConfig} from '../app/web-socket/web-socket.config';
 export const environment = {
   production: false,
   apiUrl: '',
-  ws: {url: 'ws://localhost:9666/ws'} as WebSocketConfig,
+  ws: {url: 'ws://localhost:9666/websocket'} as WebSocketConfig,
   im: {
     magic: 0xCAFEBABE, // 魔数
     version: 1 // 版本号
